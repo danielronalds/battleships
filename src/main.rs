bdffaadbe6905c4ba76a99989d860e0f8d83d3e1
Daniel R <nzdanron@gmail.com>
2023-01-27 21:18:12 +1300

    Finished the letter_to_coordinate_function and got parsing of the Chess style notation for guesses working. No error handling as of right now

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
-use battleships::{grid::Grid, point::Point };
+use battleships;
 
 fn main() {
-    let _points = vec![
-        Point::new(0, 1),
-        Point::new(0, 2),
-        Point::new(0, 3),
-        Point::new(0, 4),
-
-        Point::new(5, 3),
-        Point::new(4, 3),
-        Point::new(3, 3),
-    ];
-
     battleships::new_game();
 }
